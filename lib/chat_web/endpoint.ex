d4976fb713831893d54e9d32a01569d23c6b2bb4
Steven Hedges <steven.hedges34@gmail.com>
2024-09-22 18:58:49 -0400

    Have basic chat window

--- lib/chat_web/endpoint.ex
+++ lib/chat_web/endpoint.ex
@@ -15,6 +15,11 @@ defmodule ChatWeb.Endpoint do
     websocket: [connect_info: [session: @session_options]],
     longpoll: [connect_info: [session: @session_options]]
 
+  # Add socket handler
+  socket "/socket", ChatWeb.UserSocket,
+    websocket: true,
+    longpoll: false
+
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
